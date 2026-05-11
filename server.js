b443ea70af907f95b5fc90680411af4daeb6b4cf
Alpha-Ibrahim01 <muhammad.ibrahim71205@gmail.com>
2026-05-11 11:42:11 +0500

    updated backend

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 
 const app = express();
 app.use(cors());
